71f07398dab782c19270fb0795099e3eee099239
Débora Barros <debora_lbarros@outlook.com>
2020-09-23 12:00:36 -0400

    Idenficaçao do turno dos jogadores

--- js/script.js
+++ js/script.js
@@ -46,6 +46,14 @@ const criarBolinhas = (jogador) => {
   return bolinha; // Jefte - Retorna a Div "bolinha" na cor correspondente ao jogador
 };
 
+// Débora - Pega os spans dos jogadores, no HTML
+const spanJogador1 = document.getElementById("jogador1");
+const spanJogador2 = document.getElementById("jogador2");
+
+// Débora - Começa com o jogador1, então, adiciona a 
+// classe para identificação do turno do jogador, que neste caso, é do jogador 1
+spanJogador1.classList.add("jogadorAtual");
+
 // Jefte - Função que adiciona as bolinhas na tela //
 const adicionarBolinha = (evt) => {
   if (evt.target.className !== "cellContainer") {
@@ -75,5 +83,15 @@ const adicionarBolinha = (evt) => {
 
   //Caso o jogador tenha o valor 1, será modificado para 2, caso contrario, para 1
   jogador = jogador === 1 ? 2 : 1;
+
+  // Débora - verifica qual é o jogador atual, tira a classe de identificação de turno do outro jogador
+  // e atribui a classe de identificação ao jogador atual
+  if (jogador === 1) {
+    spanJogador2.classList.remove("jogadorAtual");
+    spanJogador1.classList.add("jogadorAtual");
+  } else {
+    spanJogador1.classList.remove("jogadorAtual");
+    spanJogador2.classList.add("jogadorAtual");
+  }
 };
 gridDivPai.addEventListener("click", adicionarBolinha); // Jefte - Adiciona um EventHandler que aciona a função de adicionar as Bolinhas na tela //
